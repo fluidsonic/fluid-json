d5cb43193b4d25d18f27b864e4675d72dc9eec59
Marc Knaup <marc@knaup.koeln>
2017-10-07 08:03:10 +0200

    Removed no-arg JSONException constructor

--- Sources/JSONException.kt
+++ Sources/JSONException.kt
@@ -4,11 +4,4 @@ import org.apiguardian.api.API
 
 
 @API(status = API.Status.EXPERIMENTAL)
-class JSONException : RuntimeException {
-
-	@API(status = API.Status.EXPERIMENTAL)
-	constructor()
-
-	@API(status = API.Status.EXPERIMENTAL)
-	constructor(message: String) : super(message)
-}
+class JSONException constructor(message: String) : RuntimeException(message)
