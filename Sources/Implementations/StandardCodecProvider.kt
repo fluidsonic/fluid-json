2457ecf2d41fd0b523b7652ea2b75b01a3d209b3
Marc Knaup <marc@knaup.koeln>
2017-10-31 21:10:43 +0100

    Performance improvements in StandardCodecProvider

--- Sources/Implementations/StandardCodecProvider.kt
+++ Sources/Implementations/StandardCodecProvider.kt
@@ -1,5 +1,6 @@
 package com.github.fluidsonic.fluid.json
 
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
 
 
@@ -7,56 +8,38 @@ internal class StandardCodecProvider<in Context : JSONCoderContext>(
 	providers: Iterable<JSONCodecProvider<Context>>
 ) : JSONCodecProvider<Context> {
 
-	private val providers = providers.toSet()
+	private val providers = providers.toSet().toTypedArray()
 
-	private val decoderCodecByType = mutableMapOf<JSONCodableType<*>, JSONDecoderCodec<*, Context>>()
-	private val encoderCodecByClass = mutableMapOf<KClass<*>, JSONEncoderCodec<*, Context>>()
+	private val decoderCodecByType = ConcurrentHashMap<JSONCodableType<*>, JSONDecoderCodec<*, Context>>()
+	private val encoderCodecByClass = ConcurrentHashMap<KClass<*>, JSONEncoderCodec<*, Context>>()
 
 
-	@Suppress("UNCHECKED_CAST")
+	@Suppress("LoopToCallChain", "UNCHECKED_CAST")
 	override fun <Value : Any> decoderCodecForType(decodableType: JSONCodableType<in Value>): JSONDecoderCodec<out Value, Context>? {
-		val codecs = decoderCodecByType
-
-		var decoder = synchronized(codecs) { codecs[decodableType] }
-		if (decoder != null) {
-			return decoder as JSONDecoderCodec<out Value, Context>?
-		}
-
-		for (provider in providers) {
-			decoder = provider.decoderCodecForType(decodableType)
-			if (decoder != null) {
-				synchronized(codecs) {
-					codecs[decodableType] = decoder!! // https://youtrack.jetbrains.com/issue/KT-7186
+		return decoderCodecByType.getOrPut(decodableType) {
+			for (provider in providers) {
+				val decoder = provider.decoderCodecForType(decodableType)
+				if (decoder != null) {
+					return@getOrPut decoder
 				}
-
-				return decoder
 			}
-		}
 
-		return null
+			return null
+		} as JSONDecoderCodec<out Value, Context>
 	}
 
 
-	@Suppress("UNCHECKED_CAST")
+	@Suppress("LoopToCallChain", "UNCHECKED_CAST")
 	override fun <Value : Any> encoderCodecForClass(encodableClass: KClass<out Value>): JSONEncoderCodec<in Value, Context>? {
-		val codecs = encoderCodecByClass
-
-		var encoder = synchronized(codecs) { codecs[encodableClass] }
-		if (encoder != null) {
-			return encoder as JSONEncoderCodec<Value, Context>?
-		}
-
-		for (provider in providers) {
-			encoder = provider.encoderCodecForClass(encodableClass)
-			if (encoder != null) {
-				synchronized(codecs) {
-					codecs[encodableClass] = encoder!! // https://youtrack.jetbrains.com/issue/KT-7186
+		return encoderCodecByClass.getOrPut(encodableClass) {
+			for (provider in providers) {
+				val encoder = provider.encoderCodecForClass(encodableClass)
+				if (encoder != null) {
+					return@getOrPut encoder
 				}
-
-				return encoder
 			}
-		}
 
-		return null
+			return null
+		} as JSONEncoderCodec<in Value, Context>
 	}
 }
