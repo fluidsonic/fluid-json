03aac8f80dc4b7ee0a3a9f1fb66f8e444152cc90
Marc Knaup <marc@knaup.io>
2019-03-03 22:09:47 +0100

    replace deprecated Class.newInstance()

--- coding/sources/CodingImplementationsJava.kt
+++ coding/sources/CodingImplementationsJava.kt
@@ -12,7 +12,8 @@ internal interface CodingImplementationsJava {
 
 internal val codingImplementationsJava: CodingImplementationsJava by lazy {
 	try {
-		return@lazy Class.forName("com.github.fluidsonic.fluid.json.dynamic.CodingImplementationsJava8").newInstance() as CodingImplementationsJava
+		return@lazy Class.forName("com.github.fluidsonic.fluid.json.dynamic.CodingImplementationsJava8").getDeclaredConstructor().newInstance()
+			as CodingImplementationsJava
 	}
 	catch (e: Exception) {
 		// non-existent or severely broken
