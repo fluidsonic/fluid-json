accd358687a1247785366625341eeb7ca6be0b99
Marc Knaup <marc@knaup.io>
2020-08-25 13:26:07 +0100

    🎉 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.8"
 }
 
-fluidLibrary(name = "json", version = "1.1.0") {
+fluidLibrary(name = "json", version = "1.1.1") {
 	allModules {
 		publishSingleTargetAsModule()
 	}
