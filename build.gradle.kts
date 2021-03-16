e8da076becdd219717297d0352f1971556ae930b
Marc Knaup <marc@knaup.io>
2021-03-16 18:28:21 +0100

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.21"
+	id("io.fluidsonic.gradle") version "1.1.22"
 }
 
 fluidLibrary(name = "json", version = "1.2.0")
