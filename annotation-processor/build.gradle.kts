6f8cbe919a7c61172a7d2f54babd04c7ae08561a
Marc Knaup <marc@knaup.io>
2022-02-02 00:40:59 +0100

    Disable KT-45545 workaround for Java 8

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -26,17 +26,18 @@ fluidLibraryModule(description = "A JSON library written in pure Kotlin (annotat
 }
 
 // https://youtrack.jetbrains.com/issue/KT-45545
-tasks.withType<Test>().all {
-	jvmArgs(
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-		"--add-opens", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-	)
-}
+if (JavaVersion.current().isJava9Compatible)
+	tasks.withType<Test>().all {
+		jvmArgs(
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+			"--add-opens", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+		)
+	}
